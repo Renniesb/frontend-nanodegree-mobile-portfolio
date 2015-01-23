010a114435a1cfead2f1d4fb4824369c49ed4bab
Rennie Bevineau <rsbevineau@gmail.com>
2015-01-23 19:10:37 +0100

    change the translate call

--- views/js/main.js
+++ views/js/main.js
@@ -534,7 +534,7 @@ function updatePositions() {
    
   for (var i = 0, itemsLength= items.length; i < itemsLength; i++) {
     var phase = Math.sin(phaseNumber  + (i % 5));
-    items[i].style.transform = "translateX(items[i].basicLeft + 100 * phase + 'px')" ;
+    items[i].style.transform = "translateX(+ 100 * phase + 'px')" ;
   }
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
